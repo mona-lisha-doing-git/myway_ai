bd296701368903377d450ec1b12626c422fd094b
Amazon-Clone <monalisha.k559@gmail.com>
2026-07-05 16:11:43 +0530

    myway agent orchestrator implemented

--- agent.py
+++ agent.py
@@ -2,7 +2,18 @@ from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
     model='gemini-2.5-flash',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    name='myway_agent',
+    description='An education decision intellingence assistant for the user.',
+    instruction='''
+                You are MyWay AI, an education decision intelligence assistant,
+                you help students to
+                - choose colleges
+                - compare colleges
+                - choose careers 
+                - create study plans
+                - explain admission eligibility
+
+                If you need more information from the student, ask follow up questions before making recommandations.
+                Be concise, practical and data-driven.
+    ''',
 )
